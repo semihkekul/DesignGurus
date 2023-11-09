5ff7e248f340b250e410b098266d38d456d07021
Semih Kekul <semih.kekul@philips.com>
2023-11-09 15:49:08 +0100

    calismiyor daha

--- Knapsack/0_1 Knapsack.cpp
+++ Knapsack/0_1 Knapsack.cpp
@@ -1,4 +1,3 @@
-
 class Solution {
   public:
 
@@ -6,7 +5,8 @@ class Solution {
                   int index, 
                   int capacityLeft,
                   int profitSum,
-                  int &maxProfit) {
+                  int &maxProfit,
+                  vector<vector<int>>& DP) {
       string padding(index, '-');              
       std::cout <<padding<<"index: " <<index << " capLeft: " << capacityLeft << " p.Sum: " <<
               profitSum << " maxProfit: " << maxProfit << "\n";             
@@ -14,29 +14,44 @@ class Solution {
       {
         return;
       } 
+    
       int w = weights[index];
       int p = profits[index];
 
+     
+
+
+      if(DP[index][capacityLeft] != -1)
+      {
+        std::cout << "found " << DP[index][capacityLeft - w] << "\n";
+        return;
+      }
+
       if(capacityLeft - w >= 0 )
       {
+
+
         maxProfit = max(maxProfit, profitSum + p);
+        recursion(profits, weights, index + 1, 
+                              capacityLeft - w, profitSum + p, maxProfit, DP);
+
+        
+
       }
       
       recursion(profits, weights, index + 1, 
-                              capacityLeft - w, profitSum + p, maxProfit);
-
-      recursion(profits, weights, index + 1, 
-                            capacityLeft, profitSum, maxProfit);
+                            capacityLeft, profitSum, maxProfit, DP);
       
-
+      DP[index][capacityLeft] = maxProfit;
     }
 
 
     int solveKnapsack(vector<int> &profits, vector<int> &weights, int capacity) {
-      int maxProfit = -1;
- 
+      int maxProfit = 0;
+      vector<vector<int>> DP(profits.size(), vector<int>(capacity + 1, -1));
+      std::cout <<"\n";
       recursion(profits, weights, 0, 
-                            capacity, 0, maxProfit);
+                            capacity, 0, maxProfit, DP);
  
       return maxProfit;
     }
